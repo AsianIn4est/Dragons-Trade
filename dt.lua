ab8f7f7492bbd819aae48fd7d6f28cd7e9996b3e
AsianIn4est <avsv3@ya.ru>
2018-06-17 12:37:59 +0300

    нарисовал кнопочки

--- dt.lua
+++ dt.lua
@@ -8,93 +8,138 @@ autor: Asian_In4est
 local args = ...
 local os = require("os")
 local mode = ""
-local debug = false
-local gpu, event, component, sides, colors
+local gpu
+local component = require("component")
+local event = require("event")
+local sides = require("sides")
+local colors
+local cfg = {
+    gui_sleep = 0.1, -- задержка в оконной функции
+    dev = true, -- флаг вывода дополнительной инфы
+    max_x = 0,
+    max_y = 0
+}
 ----------------------------------------------------------------------------------------------------
-
-if type(args) == "string" then
-    mode = args[0]
-    if mode == "gui" then
-        -- режим торг. автомата
-        gpu = component.gpu
-
-    elseif mode == "server" then
-        -- режим сервака
-    elseif mode == "unit" then
-        -- режим разработчика
-        debug = true
-    else
-        print("Bad argument. Usage dt type (gui or server)")
-        os.exit()
-    end
-else
-    print("No argument. Usage: $ dt <type>. Bye!")
-    os.exit()
-end
-
-----------------------------------------------------------------
---  GUI 
-----------------------------------------------------------------
-
 -- кнопки для главного экрана
-local buttons_cfg{
+local buttons_cfg = {
     sX = 110,
     sY = 6,
     W = 40,
-    H = 6
+    H = 6,
+    SP = 2 -- space между кнопками
 }
+
+-- слой для показа на главном экране
+local layer
 
 local buttons = {
     {
         -- имя кнопки
         name = "btn_login", 
         -- надпись
-        text = "Логин",
+        label = "Логин",
         cords = {}, 
         --     фон: ВКЛ       ВЫКЛ      ВКЛ      ВЫКЛ     
         colors = {0xFFDEAD, 0xFFF8DC, 0x000000, 0x000000},
         -- обработчик кликов
         call = btn_login_onClick,
         -- права доступа
-        perm = "NOLOGIN"
+        perm = "NOLOGIN",
+        enable = true
     },
     {
         name = "btn_sell", 
-        text = "Продать",
+        label = "Продать",
         cords = {}, 
         colors = {0xFFDEAD, 0xFFF8DC, 0x000000, 0x000000},
         call = btn_sell_onClick,
-        perm = "USER"
+        perm = "USER",
+        enable = true,
     },  
     {
         name = "btn_buy", 
-        text = "Купить",
+        label = "Купить",
         cords = {}, 
         colors = {0xFFDEAD, 0xFFF8DC, 0x000000, 0x000000},
         call = btn_sell_onClick,
-        perm = "USER"
+        perm = "USER",        
+        enable = true
     }, 
     {
         name = "btn_lk", 
-        text = "Личный кабинет",
+        label = "Личный кабинет",
         cords = {}, 
         colors = {0xFFDEAD, 0xFFF8DC, 0x000000, 0x000000},
         call = btn_sell_onClick,
-        perm = "USER"
+        perm = "USER",
+        enable = true
     },
     {
         name = "btn_admin", 
-        text = "Админка",
+        label = "Админка",
         cords = {}, 
         colors = {0xFFDEAD, 0xFFF8DC, 0x000000, 0x000000},
         call = btn_admin_onClick,
-        perm = "ADMIN"
+        perm = "ADMIN",
+        enable = false
     }   
 }
 
-function mode_gui()
 
+----------------------------------------------------------------
+--  GUI 
+----------------------------------------------------------------
+
+function init_gui()
+-- вызывается перед началом оконного цикла
+    gpu = component.gpu
+    layer = "main"
+    cfg.max_x, cfg.max_y = gpu.getResolution()
 end
+
+function mode_gui()
+-- оконный цикл
+    clear_gui()
+    if layer == "main" then
+        for k, b in pairs(buttons) do
+            local oldb, oldf = gpu.getBackground(), gpu.getForeground()
+            -- рисуем наши кнопочки
+            if b.enable then
+                gpu.setBackground(b.colors[1])
+                gpu.setForeground(b.colors[3])
+            else
+                gpu.setBackground(b.colors[2])
+                gpu.setForeground(b.colors[4])                   
+            end
+
+            b.cords.x = buttons_cfg.sX
+            b.cords.y = buttons_cfg.sY + ((k-1)*buttons_cfg.H)
+            b.cords.x2 = buttons_cfg.sX + buttons_cfg.W
+            -- спейсик между кнопочками )))
+            if k > 1 then b.cords.y2 = b.cords.y2 + buttons_cfg.SP end
+            gpu.fill(b.cords.x, b.cords.y, buttons_cfg.W, buttons_cfg.H, " ")
+            gpu.set(b.cords.x + 2, b.cords.y+buttons_cfg.H/2, b.label)
+            gpu.setBackground(oldb)
+            gpu.setForeground(oldf)
+
+            print("{"..k.."} " .. b.name .. ">>" .. b.perm)
+        end
+
+    end
+    event_gui()
+end
+
+function event_gui()
+-- обрабатываем события
+    local _ = event.pull("touch")
+end
+
+
+function clear_gui()
+-- очистка экрана
+    gpu.fill(1,1, cfg.max_x, cfg.max_y, " ")
+end
+
 
 function btn_login_onClick()
 
@@ -119,5 +164,33 @@ end
 
 
 
+----------------------------------------------------------------------------------------------------
+-- void main () {
+----------------------------------------------------------------------------------------------------
+if type(args) == "string" then
+    mode = args
+    -- режим торг. автомата
+    if mode == "gui" then
+            init_gui()
+            while 1 do
+                -- если режим работы gui вызываем оконную функцию
+                mode_gui()
+                os.sleep(cfg.gui_sleep)
+            end
+    elseif mode == "server" then
+        -- режим сервака
+    elseif mode == "unit" then
+        -- режим разработчика
+        debug = true
+    else
+        print("Bad argument. Usage dt type (gui or server)")
+        os.exit()
+    end
+else
+    print("No argument. Usage: $ dt <type>. Bye!")
+    os.exit()
+end
 
-
+----------------------------------------------------------------------------------------------------
+-- }
+----------------------------------------------------------------------------------------------------
